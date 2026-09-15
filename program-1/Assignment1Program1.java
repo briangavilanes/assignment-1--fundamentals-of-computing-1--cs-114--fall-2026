bb5fbacf571407c4c462530475e4034396296b35
briangavilanes <briang06810@gmail.com>
2026-09-15 13:42:46 -0400

    Add program

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    // 5-character string
+    String chartext;
+    String reversedtext = "";
+    Scanner scan = new Scanner(System.in);
 
+    System.out.println("Character String | Enter a 5 Character string:");
+    chartext = scan.nextLine().substring(1,4);
+
+    for (int i = chartext.length() - 1; i >= 0; i--) {
+      reversedtext += chartext.charAt(i);
+    }
+
+    System.out.println("Entered: \"" + reversedtext + "\"");
+
+    // Fahrenheit to Celsius conversion
+    float Celsius;
+    Scanner convertscan = new Scanner(System.in);
+
+    System.out.println("Convert to Celsius | Enter a number in Fahrenheit:");
+    float conversion = (convertscan.nextFloat() - 32) * 5/9;
+
+    Celsius = conversion;
+
+    System.out.println("Entered: \"" + Celsius + "\"");
+
+    // Random Number
+    Random gen = new Random();
+    int numberpicked;
+
+    numberpicked = gen.nextInt(16384) + 32;
+    System.out.println("Random number picked: \""+ numberpicked +"\"");
+
+    System.out.println("Final String: \"" + Celsius + reversedtext + numberpicked +"\"");
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    int baseinput;
+    Scanner inputscan = new Scanner(System.in);
+    System.out.println("|| Base Conversion Program ||\n|| Please enter a base (2-9)");
 
+    baseinput = inputscan.nextInt();
+    int maxnumber = (int) Math.pow(baseinput, 4) - 1;
+
+    System.out.println("The maximum, 4-digit, base 10 number in base \""+ baseinput + "\" is \""+ maxnumber +"\"");
+    System.out.println("Enter a base 10 number in the range 0 to \"" + maxnumber + "\" to convert");
+
+    int baseconvert = inputscan.nextInt();
+
+    int digit1 = baseconvert / ((int) Math.pow(baseinput, 3));
+    int digit2 = (baseconvert / ((int) Math.pow(baseinput, 2))) % baseinput;
+    int digit3 = (baseconvert / baseinput) % baseinput;
+    int digit4 = baseconvert % baseinput;
+
+    System.out.println(baseconvert + " (base 10) = " + digit1 + digit2 + digit3 + digit4 + " (base " + baseinput +")");
   }
 }
